603353ca7155cf7673950bd4e942c806c3d9186c
joyarnold16 <joy.arnold16@gmail.com>
2026-07-25 16:33:53 +0530

    feat: add ROR WebView activity

---
+++ ror-colreg/app/src/main/java/com/joyarnold/rorvisualdeck/MainActivity.java
@@ -0,0 +1,41 @@
+package com.joyarnold.rorvisualdeck;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public final class MainActivity extends Activity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setStatusBarColor(Color.rgb(7, 17, 31));
+        getWindow().setNavigationBarColor(Color.rgb(7, 17, 31));
+
+        webView = new WebView(this);
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setAllowFileAccess(true);
+        settings.setAllowContentAccess(false);
+        settings.setBuiltInZoomControls(false);
+        settings.setDisplayZoomControls(false);
+        webView.setWebViewClient(new WebViewClient());
+        webView.setBackgroundColor(Color.rgb(7, 17, 31));
+        webView.loadUrl("file:///android_asset/index.html");
+        setContentView(webView);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView != null && webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
