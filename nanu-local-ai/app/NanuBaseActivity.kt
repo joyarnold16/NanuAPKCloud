ebe203a51149d929169ea7912fe44cdfec3fc41a
joyarnold16 <joy.arnold16@gmail.com>
2026-08-22 07:55:02 +0530

    Add Talk and Create navigation

--- nanu-local-ai/app/NanuBaseActivity.kt
+++ nanu-local-ai/app/NanuBaseActivity.kt
@@ -6,6 +6,16 @@ import androidx.appcompat.app.AppCompatActivity
 
 open class NanuBaseActivity : AppCompatActivity() {
     @Suppress("UNUSED_PARAMETER")
+    fun openTalk(view: View) {
+        startActivity(Intent(this, TalkActivity::class.java))
+    }
+
+    @Suppress("UNUSED_PARAMETER")
+    fun openCreate(view: View) {
+        startActivity(Intent(this, CreateActivity::class.java))
+    }
+
+    @Suppress("UNUSED_PARAMETER")
     fun openTrading(view: View) {
         startActivity(Intent(this, TradingActivity::class.java))
     }
